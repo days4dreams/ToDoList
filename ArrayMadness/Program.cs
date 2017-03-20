83e25c06b69eb18487ab2540c8fc0883507dc77a
Lauren Wilson <emaillaurenwilson@gmail.com>
2017-03-20 13:21:55 +0000

    Update Program.cs
    Notes: 
    With lines 72 - 78 commented out, I am able to save a copy of the file to a new location using the File.Copy method.
    
    However, I am getting into a muddle when asking the user to input the file. If you comment these back in and use the string targetPathUser in place of targetPath; various exceptions are thrown to indicate I either don't have permission, the file does not exist or that it reads C:\\users\\file.
    
    I am also unsure whether line 78 'newPlace = "@" + targetPathUser;' is the correct way to attempt to append the @ to the path the user inputs. I have done this on the assumption that the user will not be aware of escape characters in order to type \\.
    
    The 'done' user input ideally would not be added to the list of to-dos. I am unsure how to ignore this, perhaps do something with an array, and remove the last item....

--- ArrayMadness/Program.cs
+++ ArrayMadness/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,9 +13,9 @@ namespace ArrayMadness
         static void Main(string[] args)
         {
 
-            string file = "C:\\MyToDoList.txt";
+            string file = "C:\\MyFile.txt";
             // a single backslash in a special character. Using two to indicate a single \ for file path
-            string title = "To Do List\n";
+            string title = "test";
             File.WriteAllText(file, title);
             // creates a text file in the location of string file and puts the string contents in it
 
@@ -34,8 +34,8 @@ namespace ArrayMadness
                 decision = yesAnswers.Contains(newItem.ToLower());  //now use ToLower so that I convert the value to be all lower case. I don't have to worry about uppercase now. 
             if (decision == false)// whilst the anser is a no, do this
             {
-                Console.WriteLine("Suit yourself!\n");
-                Console.WriteLine("\nHere is how your todo list looks:\n");
+                Console.WriteLine("\nOK!");
+                Console.WriteLine("\nHere is how your To Do List looks:\n");
                 foreach (string entry in list)
                 {
                     Console.WriteLine(entry);
@@ -57,23 +57,44 @@ namespace ArrayMadness
                     foreach (string stringValues in array)
                     {
                         Console.WriteLine(stringValues);
-                    }
+                        File.WriteAllText(file, stringValues);
+                    //converts list to string, pushes to text file
+                }
+                
             }
-
-            string[] array2 = list.ToArray();
-            foreach (string stringValues in array2)
-            File.WriteAllText(file, stringValues);
-            //converts list to string, pushes to text file
 
             string content = File.ReadAllText(file);
             Console.WriteLine(content);
             // looks for the file in location string file and prints all of the text to the console
 
             // add in logic for saving a copy
+            string targetPath = @"C:\Users\";
+            //Console.WriteLine("\nThis looks like important info! Let's make a backup.");
+            //Console.WriteLine("Where would you like to save a copy of your To Do List? Please include the full path.");
+            //Console.WriteLine("EG. C:\\Users");
+            //string targetPathUser = string.Empty;
+            //targetPathUser = Console.ReadLine();
+            //string newPlace = "";
+            //newPlace = "@" + targetPathUser;
 
+
+            // Use Path class to manipulate file and directory paths.
+            string sourceFile = @"C:\MyFile.txt";
+            string destFile = Path.Combine(targetPath, title);
+
+            // To copy a folder's contents to a new location:
+            // Create a new target folder, if necessary.
+            if (!Directory.Exists(targetPath))
+            {
+                Directory.CreateDirectory(targetPath);
+            }
+
+            // To copy a file to another location and 
+            // overwrite the destination file if it already exists.
+            File.Copy(sourceFile, destFile);
 
             // access file info
-            FileInfo ToDoListInfo = new FileInfo("C:\\MyToDoList.txt");
+            FileInfo ToDoListInfo = new FileInfo("C:\\MyFile.txt");
             // we can call the class FileInfo because it existing the .Net framework library (its not a complex object)
             // we create a new object and tell it to point to the path of MyToDoList.txt
 
@@ -81,7 +102,7 @@ namespace ArrayMadness
             DateTime fileCreated = ToDoListInfo.CreationTime;
             // capture information for the attriibutes of our To Do List
 
-            Console.WriteLine("Your To Do List is " + fileLength + " characters in length");
+            // Console.WriteLine("\nYour To Do List is " + fileLength + " characters in length"); this is not working
             Console.WriteLine("Your To Do List was created at " + fileCreated);
             // would be nice to add an item count / line count so user knows how my items are in to do list
 
@@ -90,3 +111,4 @@ namespace ArrayMadness
         }
     }
 }
+
